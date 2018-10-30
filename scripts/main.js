fd8084bc23173ac62a24b84278adf941ced634d9
lzc1994 <32731835+lzc1994@users.noreply.github.com>
2018-10-30 21:21:57 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+﻿var myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/Sophie.jpg') {
+        myImage.setAttribute('src', 'images/Sophie3.jpg');
+    }
+    else if (mySrc === 'images/Sophie3.jpg') {
+        myImage.setAttribute('src', 'images/Sophie2.jpg');
+    }
+    else if (mySrc === 'images/Sophie2.jpg') {
+        myImage.setAttribute('src', 'images/Sophie4.jpg');
+    }
+    else{
+        myImage.setAttribute('src', 'images/Sophie.jpg');
+    }
+}
+/*
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h2');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'So YOU MUST BE Crazy, ' + myName;
+}
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'So YOU MUST BE Crazy, ' + storedName;
+}
+myButton.onclick = function () {
+    setUserName();
+}
+*/
